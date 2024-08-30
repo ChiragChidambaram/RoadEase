399b5ae202c7f36cc70215a4ff44cad251d77444
@reddy-bv <sudharshana.reddy@rhibhus.com>
2024-08-31 00:24:46 +0530

    new pages

---
+++ src/components/AuthorityScreen.js
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Header from './Header';
+import FeedPage from '../view/FeedPage';
+
+const Tab = createBottomTabNavigator();
+
+export default function AuthorityScreen() {
+  return (
+    <Tab.Navigator
+      initialRouteName='FeedScreen'
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: '#f8f9fa',  // Custom background color for the tab bar
+          borderTopWidth: 1,
+          borderTopColor: '#e0e0e0',
+          height: 60,  // Adjust the height of the tab bar
+        },
+        tabBarLabelStyle: {
+          fontSize: 16,  // Custom font size for tab labels
+          fontWeight: 'bold',  // Custom font weight for tab labels
+          textAlign: 'center',  // Center the text horizontally
+          alignSelf: 'center',  // Center the text vertically
+          justifyContent:"center",
+          marginBottom: 20,  // Remove margin to center the text vertically
+          flex: 1,  // Take up full space
+        },
+        tabBarActiveBackgroundColor: '#112D4E',  // Background color for the active tab
+        tabBarInactiveBackgroundColor: "#D9D9D9",  // Background color for inactive tabs
+        tabBarActiveTintColor: '#fff',  // Text color for the active tab
+        tabBarInactiveTintColor: '#333',  // Text color for inactive tabs
+        tabBarIcon: () => null,  // Remove the icon
+      }}
+    >
+      <Tab.Screen
+        name="Issues"
+        component={FeedPage}
+        options={({ navigation }) => ({
+          header: () => <Header title="Issues" navigation={navigation} />,
+        })}
+      />
+
+      <Tab.Screen
+        name="Insights"
+        component={FeedPage}
+        options={({ navigation }) => ({
+          header: () => <Header title="Insights" navigation={navigation} />,
+        })}
+      />
+
+    <Tab.Screen
+        name="Map"
+        component={FeedPage}
+        options={({ navigation }) => ({
+          header: () => <Header title="Map" navigation={navigation} />,
+        })}
+      />
+    </Tab.Navigator>
+  );
+}
--- src/components/NavigationContainer.js
+++ src/components/NavigationContainer.js
@@ -3,20 +3,32 @@ import { createStackNavigator } from '@react-navigation/stack';
 import CreateAccount from '../view/CreateAccount';
 import FrontScreen from '../view/FrontScreen';
 import Login from '../view/login';
-import CustomCard from './Card';
 import ReportPage from '../view/ReportPage';
+
+//UserFeedScreen
+import UserFeedScreen from './UserFeedScreen';
+
+//AuthorityScreen
+import AuthorityScreen from './AuthorityScreen';
+import IssuePreview from '../view/IssuePreview';
+import Header from './Header';
+import BackPage from './BackPage';
 
 const Stack = createStackNavigator();
 
 export const AuthStack =()=>{
 
   return(
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName='IssuePreview'>
         <Stack.Screen name="Home" component={FrontScreen}  options={{ headerShown: false }}/>
         <Stack.Screen name="CreateAccount" component={CreateAccount}  options={{ headerShown: false }}/>
         <Stack.Screen name="Login" component={Login}  options={{ headerShown: false }}/>
-        <Stack.Screen name="Card" component={CustomCard}  options={{ headerShown: false }}/>
         <Stack.Screen name="ReportScreen" component={ReportPage}  options={{ headerShown: false }}/>
+        <Stack.Screen name="UserFeedScreen" component={UserFeedScreen}  options={{ headerShown: false }}/>
+        <Stack.Screen name="AuthorityScreen" component={AuthorityScreen}  options={{ headerShown: false }}/>
+        <Stack.Screen name="IssuePreview" component={IssuePreview}  options={({ navigation, route }) => ({
+          header: () => <BackPage title="Home" navigation={navigation} />,
+        })}/>
       </Stack.Navigator>
   )
 }
